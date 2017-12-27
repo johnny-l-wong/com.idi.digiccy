d0f9f7eaea52b82685bffd0bca1337a38fff0e1d
Johnny L <johnny.l.wang@hotmail.com>
2017-12-27 19:28:32 +0800

    initial

--- src/IDI.Digiccy.Common/Enums/TranType.cs
+++ src/IDI.Digiccy.Common/Enums/TranType.cs
@@ -6,12 +6,12 @@
     public enum TranType
     {
         /// <summary>
-        /// 买
-        /// </summary>
-        Bid=1,
-        /// <summary>
         /// 卖
         /// </summary>
-        Ask=2
+        Ask = 0,
+        /// <summary>
+        /// 买
+        /// </summary>
+        Bid =1
     }
 }
--- src/IDI.Digiccy.Models/Transaction/TradeInput.cs
+++ src/IDI.Digiccy.Models/Transaction/TradeInput.cs
@@ -1,16 +1,21 @@
 ﻿using IDI.Core.Common;
 using IDI.Digiccy.Common.Enums;
+using Newtonsoft.Json;
 
 namespace IDI.Digiccy.Models.Transaction
 {
     public class TradeInput: IInput
     {
+        [JsonProperty("uid")]
         public int UID { get; set; }
 
+        [JsonProperty("tran_type")]
         public TranType Type { get; set; }
 
+        [JsonProperty("price")]
         public decimal Price { get; set; }
 
+        [JsonProperty("size")]
         public decimal Size { get; set; }
     }
 }
